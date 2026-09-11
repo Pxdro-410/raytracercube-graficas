22885d501366260aa6c25a26ef36f1a673b59973
Pxdro-410 <pedrojuliocaso@gmail.com>
2026-09-10 20:01:23 -0600

    feat: definir traits de intersección, materiales y luz

---
+++ src/ray_intersect.rs
@@ -0,0 +1,25 @@
+use crate::color::Color;
+use nalgebra_glm::Vec3;
+
+#[derive(Debug, Clone, Copy)]
+pub struct Material {
+    pub diffuse: Color,
+}
+
+impl Material {
+    pub fn new(diffuse: Color) -> Self {
+        Material { diffuse }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Intersect {
+    pub point: Vec3,
+    pub normal: Vec3,
+    pub distance: f32,
+    pub material: Material,
+}
+
+pub trait RayIntersect {
+    fn ray_intersect(&self, ray_origin: &Vec3, ray_direction: &Vec3) -> Option<Intersect>;
+}
